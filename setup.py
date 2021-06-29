1b2ce754b36f03c082b604b89dc0a9ce0d625c92
hampen2929 <mochi.no.kimochi.2929@gmail.com>
2021-06-29 17:10:08 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+import codecs
+import os
+from setuptools import setup, find_packages
+
+PACKAGE = 'onnx_infer'
+README = 'README.md'
+REQUIREMENTS = 'requirements.txt'
+
+VERSION = '0.0.1'
+
+DESCRIPTION = 'The implementation of onnx model for inference.'
+
+
+def read(fname):
+    # file must be read as utf-8 in py3 to avoid to be bytes
+    return codecs.open(os.path.join(os.path.dirname(__file__), fname),
+                       encoding='utf-8').read()
+
+setup(name=PACKAGE,
+      version=VERSION,
+      # long_description=read(README),
+      install_requires=list(read(REQUIREMENTS).splitlines()),
+      url='https://github.com/hampen2929/onnx_infer',
+      author='hampen2929',
+      author_email='yuya.mochimaru.ym@gmail.com',
+      packages=find_packages(),
+      license='Apache'
+      )
