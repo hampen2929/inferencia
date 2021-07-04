fa302c210bc0145358b03a9f17c116a6df9d0299
hampen2929 <mochi.no.kimochi.2929@gmail.com>
2021-07-04 23:51:59 +0900

    label factory for to_json

--- inferencia/task/object_detection/object_detection_2d/label/object_detection_2d_label_factory.py
+++ inferencia/task/object_detection/object_detection_2d/label/object_detection_2d_label_factory.py
@@ -1,11 +1,13 @@
 from ..label.object_detection_2d_label_name import ObjectDetection2DLabelName
+from inferencia.util.label.label_process import to_dict
 
 
 class ObjectDetection2DLabelFactory():
     def create(label_name):
         if ObjectDetection2DLabelName.coco.value == label_name:
             from ..label.label.coco_label import COCOLabel
-            return COCOLabel
+            # label_dict = to_dict(COCOLabel)
+            return COCOLabel()
 
         elif ObjectDetection2DLabelName.pascalvoc.value == label_name:
             raise NotImplementedError(
