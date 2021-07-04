4d71965f8399ba0d100552fac252d6a4fc76c215
hampen2929 <mochi.no.kimochi.2929@gmail.com>
2021-07-04 23:51:23 +0900

    obj manager args

--- inferencia/task/object_detection/object_detection_2d/object_detection_2d_manager.py
+++ inferencia/task/object_detection/object_detection_2d/object_detection_2d_manager.py
@@ -5,8 +5,18 @@ from .visualization.object_detection_2d_visualizer_factory import ObjectDetectio
 
 class ObjectDetection2DManager():
 
-    def get_model(model_name):
-        return ObjectDetection2DModelFactory.create(model_name=model_name)
+    def get_model(model_name="YoloV4Middle",
+                  model_path=None,
+                  model_precision="FP32",
+                  conf_thresh=0.2,
+                  nms_thresh=0.4,
+                  label_name="COCO"):
+        return ObjectDetection2DModelFactory.create(model_name,
+                                                    model_path,
+                                                    model_precision,
+                                                    conf_thresh,
+                                                    nms_thresh,
+                                                    label_name)
 
     def get_visualizer(visualizer_name="BBOXVisualizer"):
         return ObjectDetection2DVisualizerFactory.create(visualizer_name)
