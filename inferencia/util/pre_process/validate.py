2334d3df2d7701fa6a08c21f0a0fe0bbebae51a9
hampen2929 <mochi.no.kimochi.2929@gmail.com>
2021-07-11 13:02:30 +0900

    add valicate image function

---
+++ inferencia/util/pre_process/validate.py
@@ -0,0 +1,28 @@
+from typing import List, Union
+import numpy as np
+
+
+def validate_image(image: Union[np.ndarray, List]) -> List[np.ndarray]:
+    """input image returned as List
+
+    Args:
+        image (Union[np.ndarray, List]): single image or list multi image
+
+    Raises:
+        ValueError: [description]
+
+    Returns:
+        List[np.ndarray]: [description]
+    """
+    if isinstance(image, np.ndarray):
+        images_shape = image.shape
+        if len(images_shape) == 3:
+            "single image"
+            images = [image]
+        else:
+            msg = "image shape length must be 3. Not {}.".format(
+                len(images_shape))
+            raise ValueError(msg)
+    elif isinstance(image, list):
+        pass
+    return images
