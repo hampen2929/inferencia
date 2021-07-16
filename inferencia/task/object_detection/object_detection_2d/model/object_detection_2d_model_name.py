c187235b5376eb2decc8b00b703ab9576984fc31
hampen2929 <mochi.no.kimochi.2929@gmail.com>
2021-07-17 01:50:55 +0900

    add method ro name class

--- inferencia/task/object_detection/object_detection_2d/model/object_detection_2d_model_name.py
+++ inferencia/task/object_detection/object_detection_2d/model/object_detection_2d_model_name.py
@@ -5,4 +5,15 @@ class ObjectDetection2DModelName(Enum):
     yolo_v4 = "YoloV4"
     yolo_v4_middle = "YoloV4Middle"
     tiny_yolo_v4 = "TinyYoloV4"
-    centernet = "CenterNet"
+
+    @staticmethod
+    def to_dict():
+        return {l.name: l.value for l in ObjectDetection2DModelName}
+
+    @staticmethod
+    def names():
+        return [l.name for l in ObjectDetection2DModelName]
+
+    @staticmethod
+    def values():
+        return [l.value for l in ObjectDetection2DModelName]
