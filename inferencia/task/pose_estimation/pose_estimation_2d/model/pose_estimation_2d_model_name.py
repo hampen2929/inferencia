60a6a285bb4d2003cf0e179720517a75146be4d9
hampen2929 <mochi.no.kimochi.2929@gmail.com>
2021-07-26 08:10:02 +0900

    add methods to model name class

--- inferencia/task/pose_estimation/pose_estimation_2d/model/pose_estimation_2d_model_name.py
+++ inferencia/task/pose_estimation/pose_estimation_2d/model/pose_estimation_2d_model_name.py
@@ -5,3 +5,15 @@ class PoseEstimationModelName(Enum):
     move_net_thunder = "MoveNet-Thunder"
     move_net_lightning = "MoveNet-Lightning"
     trans_pose = "TransPose"
+
+    @staticmethod
+    def to_dict():
+        return {l.name: l.value for l in PoseEstimationModelName}
+
+    @staticmethod
+    def names():
+        return [l.name for l in PoseEstimationModelName]
+
+    @staticmethod
+    def values():
+        return [l.value for l in PoseEstimationModelName]
