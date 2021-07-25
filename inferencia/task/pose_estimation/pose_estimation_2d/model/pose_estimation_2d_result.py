34fcd5ce71adf547e5d9ffbc0e17cdf962de7899
hampen2929 <mochi.no.kimochi.2929@gmail.com>
2021-07-26 08:09:25 +0900

    set None to pose 2d ret vars

--- inferencia/task/pose_estimation/pose_estimation_2d/model/pose_estimation_2d_result.py
+++ inferencia/task/pose_estimation/pose_estimation_2d/model/pose_estimation_2d_result.py
@@ -5,31 +5,13 @@ import numpy as np
 @dataclass
 class PoseEstimation2dResult:
     pose: np.ndarray
-    pose_norm: np.ndarray
-    outputs: np.ndarray
     image_height: int
     image_width: int
+    pose_norm: np.ndarray = None
+    outputs: np.ndarray = None
     heatmap: np.ndarray = None
     query_location: np.ndarray = None
+    draw_image = None
 
     def set_draw_image(self, draw_image):
         self.draw_image = draw_image
-
-
-# class PoseEstimation2dResult:
-#     def __init__(self,
-#                  pose: np.ndarray,
-#                  pose_norm: np.ndarray,
-#                  outputs: np.ndarray,
-#                  image_height: int,
-#                  image_width: int,
-#                  image=None,
-#                  ):
-#         self.pose = pose
-#         self.pose_norm = pose_norm
-#         self.outputs = outputs
-#         self.image_height = image_height
-
-
-#     def set_draw_image(self, draw_image):
-#         self.draw_image = draw_image
